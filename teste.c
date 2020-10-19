c52f9aaebff68a32129504a99b47896e1d9588e3
vihmar <vihmar2002@hotmail.com>
2020-10-19 16:53:04 -0300

    acrescentei o teste

---
+++ teste.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+int numerosDistintos (int* v, int n) {
+int v2[n];
+int i,j, repete = 0;
+int x=0;
+
+for (i=0; i<n; i++){
+  if (i==0){
+    v2[x] = v[i];
+    x++;
+  }
+  else {
+    repete = 0;
+
+    for(j = 0; j <x; j++){
+      if(v[i] == v2[j]){
+        repete++;
+      }
+    }
+
+    if(repete<1){
+      v2[x]=v[i];
+      x++;
+
+    }
+  }
+}
+
+  return x;
+}
+
+int main(void) {
+
+  int n;
+  int i;
+  scanf("%d", &n);
+
+  int v[n];
+
+  for(i=0;  i<n  ;i++){
+    scanf("%d", &v[i]);
+  }
+
+
+ printf("%d", numerosDistintos(v, n));
+
+  return 0;
+}
